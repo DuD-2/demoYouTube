84768e0ffafd512ca66bfb780db31e129645be02
Viktor Bulatov <lightoton@gmail.com>
2024-08-07 23:17:02 +0200

    ForYouTube

--- src/main/java/com/rangers/demo/service/UserService.java
+++ src/main/java/com/rangers/demo/service/UserService.java
@@ -1,9 +1,16 @@
 package com.rangers.demo.service;
 
+import com.rangers.demo.dto.JwtAuthenticationDto;
+import com.rangers.demo.dto.RefreshTokenDto;
+import com.rangers.demo.dto.UserCredentialsDto;
 import com.rangers.demo.dto.UserDto;
 import org.springframework.data.crossstore.ChangeSetPersister;
 
+import javax.naming.AuthenticationException;
+
 public interface UserService {
+    JwtAuthenticationDto singIn(UserCredentialsDto userCredentialsDto) throws AuthenticationException;
+    JwtAuthenticationDto refreshToken(RefreshTokenDto refreshTokenDto) throws Exception;
     UserDto getUserById(String id) throws ChangeSetPersister.NotFoundException;
     UserDto getUserByEmail(String email) throws ChangeSetPersister.NotFoundException;
     String addUser(UserDto user);
--- src/main/java/com/rangers/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/rangers/demo/service/impl/UserServiceImpl.java
@@ -1,21 +1,47 @@
 package com.rangers.demo.service.impl;
 
+import com.rangers.demo.dto.JwtAuthenticationDto;
+import com.rangers.demo.dto.RefreshTokenDto;
+import com.rangers.demo.dto.UserCredentialsDto;
 import com.rangers.demo.dto.UserDto;
+import com.rangers.demo.entity.User;
 import com.rangers.demo.mapper.UserMapper;
 import com.rangers.demo.repository.UserRepository;
+import com.rangers.demo.security.jwt.JwtService;
 import com.rangers.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.naming.AuthenticationException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    public final UserRepository userRepository;
-    public final UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public JwtAuthenticationDto singIn(UserCredentialsDto userCredentialsDto) throws AuthenticationException {
+        User user = findByCredentials(userCredentialsDto);
+        return jwtService.generateAuthToken(user.getEmail());
+    }
+
+    @Override
+    public JwtAuthenticationDto refreshToken(RefreshTokenDto refreshTokenDto) throws Exception {
+        String refreshToken = refreshTokenDto.getRefreshToken();
+        if (refreshToken != null && jwtService.validateJwtToken(refreshToken)) {
+            User user = findByEmail(jwtService.getEmailFromToken(refreshToken));
+            return jwtService.refreshBaseToken(user.getEmail(), refreshToken);
+        }
+        throw new  AuthenticationException("Invalid refresh token");
+    }
 
     @Override
     @Transactional
@@ -32,8 +58,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public String addUser(UserDto user){
-        userRepository.save(userMapper.toEntity(user));
+    public String addUser(UserDto userDto){
+        User user = userMapper.toEntity(userDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
         return "User added";
+    }
+
+    private User findByCredentials(UserCredentialsDto userCredentialsDto) throws AuthenticationException {
+        Optional<User> optionalUser = userRepository.findByEmail(userCredentialsDto.getEmail());
+        if (optionalUser.isPresent()){
+            User user = optionalUser.get();
+            if (passwordEncoder.matches(userCredentialsDto.getPassword(), user.getPassword())){
+                return user;
+            }
+        }
+        throw new AuthenticationException("Email or password is not correct");
+    }
+
+    private User findByEmail(String email) throws Exception {
+        return userRepository.findByEmail(email).orElseThrow(()->
+                new Exception(String.format("User with email %s not found", email)));
     }
 }
